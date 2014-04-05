4b08a4b5f2ee1b20312c61d8ffaf66521b328713
Tom Leaman <tom@tomleaman.co.uk>
2014-04-05 17:59:28 +0100

    fix moar bugs!

--- bugsp.c
+++ bugsp.c
@@ -418,8 +418,8 @@ lval* lval_eval_sexpr(lenv* e, lval* v) {
 
     lval* f = lval_pop(v, 0);
     if (f->type != LVAL_FUN) {
-        lval* err = lval_err("S-Expression starts with incorrect type (%s)",
-                             ltype_name(f->type));
+        lval* err = lval_err("S-Expression starts with incorrect type, expected %s, got %s",
+                             ltype_name(LVAL_FUN), ltype_name(f->type));
         lval_del(v);
         lval_del(f);
         return err;
@@ -439,19 +439,33 @@ lval* lval_eval(lenv* e, lval* v) {
     }
     return v;
 }
-
 lval* lval_call(lenv* e, lval* f, lval* a) {
-    if (f->builtin) {
-        return f->builtin(e, a);
-    }
+    if (f->builtin) { return f->builtin(e, a); }
+
+    int given = a->count;
+    int total = f->formals->count;
 
     while (a->count) {
         if (f->formals->count == 0) {
             lval_del(a);
-            return lval_err("function received too many arguments");
+            return lval_err("function passed too many arguments. Got %i, Expected %i.", given, total);
         }
 
         lval* sym = lval_pop(f->formals, 0);
+
+        if (strcmp(sym->sym, "&") == 0) {
+            if (f->formals->count != 1) {
+                lval_del(a);
+                return lval_err("function format invalid. Symbol '&' not followed by single symbol.");
+            }
+
+            lval* nsym = lval_pop(f->formals, 0);
+            lenv_put(f->env, nsym, builtin_list(e, a));
+            lval_del(sym);
+            lval_del(nsym);
+            break;
+        }
+
         lval* val = lval_pop(a, 0);
         lenv_put(f->env, sym, val);
         lval_del(sym);
@@ -459,6 +473,20 @@ lval* lval_call(lenv* e, lval* f, lval* a) {
     }
 
     lval_del(a);
+
+    if (f->formals->count > 0 &&
+        strcmp(f->formals->cell[0]->sym, "&") == 0) {
+        if (f->formals->count != 2) {
+            return lval_err("Function format invalid. Symbol '&' not followed by single symbol.");
+        }
+
+        lval_del(lval_pop(f->formals, 0));
+        lval* sym = lval_pop(f->formals, 0);
+        lval* val = lval_qexpr();
+        lenv_put(f->env, sym, val);
+        lval_del(sym);
+        lval_del(val);
+    }
 
     if (f->formals->count == 0) {
         f->env->parent = e;
